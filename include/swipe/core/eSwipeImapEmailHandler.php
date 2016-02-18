f9a3439a8f428c714515a2c5cf579af1b714242b
Tyler J Barnes <b4rn3scode@gmail.com>
2016-02-18 14:08:03 -0800

    init email handler

---
+++ include/swipe/core/eSwipeImapEmailHandler.php
@@ -0,0 +1,38 @@
+<?php
+#region intro
+/***********************************************************
+ * eSwipeImapEmailHandler
+ * Handles functionality for email (message id... functions
+ * based on a singular email)
+ *
+ * @author			Tyler J Barnes
+ * @contact			tylerb@conversionvoodoo.com
+ * @version			0.0.1
+ **********************************************************/
+#endregion
+
+#region class_todo
+/*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * TODO
+ *
+ * 20160218 - Tyler J Barnes
+ * 	- init dev
+ *
+ ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+#endregion
+
+#region eSwipeImapEmailHandler
+class eSwipeImapEmailHandler {
+
+
+
+
+
+}
+#endregion
+
+
+////////////////////////////////////////////////////////////////////
+/////////////////         end          /////////////////////////////
+////////////////////////////////////////////////////////////////////
+?>
